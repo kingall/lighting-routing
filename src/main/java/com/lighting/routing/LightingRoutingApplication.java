a6558b34289048e9fddb2a11df979070b274b625
chenwei <kingall@163.com>
2022-06-23 16:10:57 +0800

    xxxxx

--- src/main/java/com/lighting/routing/LightingRoutingApplication.java
+++ src/main/java/com/lighting/routing/LightingRoutingApplication.java
@@ -2,9 +2,11 @@ package com.lighting.routing;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class LightingRoutingApplication {
     public static void main(String[] args) {
         SpringApplication.run(LightingRoutingApplication.class, args);
