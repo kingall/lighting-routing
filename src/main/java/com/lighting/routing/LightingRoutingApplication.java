19d2bd3b10b6780ba3acca1bbf744fd8e5c01744
chenwei <kingall@163.com>
2022-06-23 14:22:07 +0800

    设置完成

---
+++ src/main/java/com/lighting/routing/LightingRoutingApplication.java
@@ -0,0 +1,12 @@
+package com.lighting.routing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class LightingRoutingApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LightingRoutingApplication.class, args);
+    }
+}
